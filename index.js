70cbadc2d4c8d5032114a24ff5dd67831bc971a6
ayd823 <aydiaz823@gmail.com>
2022-07-17 09:10:18 -0400

    Completed getAllMovieTitles()

--- index.js
+++ index.js
@@ -28,7 +28,13 @@ const exampleMovies = require("./movies");
       "James and the Giant Peach",
     ];
  */
-function getAllMovieTitles() {}
+function getAllMovieTitles(movies) {
+  let array = [];
+  for (let i = 0; i < movies.length; i++) {
+    array.push(movies[i].title);
+  }
+  return array;
+}
 
 /**
  * getHighestMetascore()
