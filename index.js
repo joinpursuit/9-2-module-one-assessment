4f0da42fec2d5f8fb55a0858f63dcfbb56760127
Mohammed Islam <pluto@mohammeds-air.mynetworksettings.com>
2022-07-17 09:42:49 -0400

    solution to prob 6

--- index.js
+++ index.js
@@ -144,7 +144,15 @@ function findById(movies, id) {
  *  filterByGenre(movies, "Horror")
  *  //> []
  */
-function filterByGenre() {}
+function filterByGenre(movies, genre) {
+  let newArr = []
+  for (let movie of movies){
+    if (movie.genre.toUpperCase().includes(genre.toUpperCase())){
+      newArr.push(movie)
+    }
+  }
+  return newArr
+}
 
 /**
  * getAllMoviesReleasedAtOrBeforeYear()
