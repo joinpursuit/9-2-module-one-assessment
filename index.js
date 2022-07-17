9d9f43444589c6266aecd033867a660a9e5a0d63
i-guerrero <isaacguerrero@pursuit.org>
2022-07-17 09:46:44 -0400

    Add findById() to index.js

--- index.js
+++ index.js
@@ -110,7 +110,14 @@ function countByRating(movies) {
       // Toy Story 4
     };
  */
-function findById() {}
+function findById(movies, id) {
+  for (let movie of movies) {
+    if (movie.imdbID === id) {
+      return movie;
+    }
+  } 
+  return null;
+}
 
 /**
  * filterByGenre()
